0d29a7c537ff088efa0e19dbb41e69a14edc567a
Tabula <MrZhnex@ya.ru>
2020-07-22 16:34:28 +0300

    Приведение к общему стилю.

--- MainSettings.cs
+++ MainSettings.cs
@@ -4,20 +4,22 @@ namespace PlayersWhoPlayed
 {
     public class MainSettings : Plugin
     {
-        public override string getName => "PlayersWhoPlayed";
-        private SetEvents SetEvents;
+        public override string getName => nameof(PlayersWhoPlayed);
+        public SetEvents SetEvents { get; set; }
 
         public override void OnEnable()
         {
             SetEvents = new SetEvents();
             Events.RoundStartEvent += SetEvents.OnRoundStart;
             Events.RoundEndEvent += SetEvents.OnRoundEnd;
+            Log.Info(getName + " on");
         }
 
         public override void OnDisable()
         {
             Events.RoundStartEvent -= SetEvents.OnRoundStart;
             Events.RoundEndEvent -= SetEvents.OnRoundEnd;
+            Log.Info(getName + " off");
         }
 
         public override void OnReload() { }
